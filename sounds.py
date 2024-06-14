5d8687349a5c0714496f5c5b1c043be9bff8410f
MRamazan <mramazantrn28@gmail.com>
2024-06-14 17:09:21 +0300

    Update sounds.py

--- sounds.py
+++ sounds.py
@@ -2,13 +2,13 @@ import pygame as pg
 class sound():
     def shoot_sound(self):
         pg.mixer.init()
-        reload_sound = r"C:\Users\PC\Downloads\Gun shoot - Sound effect.mp3"
+        reload_sound = r"sounds\Gun shoot - Sound effect.mp3"
         pg.mixer.music.load(reload_sound)
         return pg.mixer.music.play()
 
     def reload_sound(self):
         pg.mixer.init()
-        reload_sound = r"C:\Users\PC\Downloads\Gun Reload sound effect (1).mp3"
+        reload_sound = r"sounds\Gun Reload sound effect (1).mp3"
         pg.mixer.music.load(reload_sound)
         return pg.mixer.music.play()
 
