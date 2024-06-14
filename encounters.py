cd9b239b57d4cb21eb1cbc88f3a8e70fdc81b903
MRamazan <mramazantrn28@gmail.com>
2024-06-14 17:04:28 +0300

    Update encounters.py

--- encounters.py
+++ encounters.py
@@ -61,6 +61,6 @@ class enc():
 
 
 if __name__ == "__main__":
-    img = pg.transform.scale(pg.image.load(r"C:\Users\PC\Downloads\853b792d9db7c1afed8be8f25694c61d.png"), (100, 200))
+    img = pg.transform.scale(pg.image.load("zombie.png"), (100, 200))
     instance = enc(img, pg.display.set_mode((1200, 1000)),random.randint(100, 800),random.randint(100, 800),1)
     instance.main()
